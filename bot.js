bc987a426b38f8b6044bfc1150b4b71f0c2cf352
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-13 13:06:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!ip') {
-    	message.channel.sendMessage('✅ IP: UnityPvP.us');
+    	message.channel.sendMessage('♦️IP: UnityPvP.us');
   	}
 });
 
