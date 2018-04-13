bef4bf922a5858f331e9daa12d448c9e28618a9e
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-13 13:12:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,17 +4,20 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+});
 
 client.on('message', message => {
     if (message.content === '!ip') {
     	message.channel.sendMessage('♦️**IP: UnityPvP.us**');
   	}
 });
+});
 
 client.on('message', message => {
     if (message.content === '!store') {
     	message.channel.sendMessege('♦️**Store: unitypvp.buycraft.net**');
   	}
+});
 });
 
 client.on('message', message => {
