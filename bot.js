a56187946ba2455ad399afc2d0badf1c6dc7cf7b
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-13 13:13:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,20 +10,6 @@ client.on('message', message => {
     	message.channel.sendMessage('♦️**IP: UnityPvP.us**');
   	}
 });
-});
-
-client.on('message', message => {
-    if (message.content === '!store') {
-    	message.channel.sendMessege('♦️**Store: unitypvp.buycraft.net**');
-  	}
-});
-});
-
-client.on('message', message => {
-    if (message.content === '!info') {
-    	message.channel.sendMessege('♦️**Coming Soon!**');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
