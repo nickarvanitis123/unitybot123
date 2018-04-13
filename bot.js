e89faa86cd7f2412f94f7db10197344374bca077
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-13 20:50:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,17 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!ip') {
-    	message.channel.sendMessage('♦️**IP: UnityPvP.us**');
-  	}
+        message.channel.sendMessage('🔻**IP: UnityPvP.us**');
+      }
+client.on('message', message => {
+    if (message.content === '!help') {
+        message.channel.sendMessage('  **Help Panel**
+Public-Commands:
+!help | Shows Bot's Help page.
+!ip | Shows the Server's IP.
+!store | Shows the Server's Store.
+UnityPvP Bot v1.3.0');
+      }
 });
 
 // THIS  MUST  BE  THIS  WAY
