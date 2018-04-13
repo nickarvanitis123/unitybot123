49046349aefc527e6f4fa8cb5dc9605c2de6b6cf
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-13 12:03:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,26 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    if (message.content === '!ip') {
+    	message.channel.sendMessege('✅ IP: UnityPvP.us');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '!store') {
+    	message.channel.sendMessege('✅ Store: unitypvp.buycraft.net');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '!info') {
+    	message.channel.sendMessege('✅ Coming Soon!');
+  	}
+});
+
+client.on('message', message => {
     if (message.content === '!bot') {
-    	message.reply('UnityPvP Bot: Coded by @TryHard#3061');
+    	message.channel.sendMessege('✅ Coded by TryHard#3061');
   	}
 });
 
