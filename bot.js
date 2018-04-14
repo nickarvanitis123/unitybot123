397482da63beda2e28404dc442e44d2d6e6365d8
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-14 16:06:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,34 @@
-const Discord = require("discord.js");
-
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const config = require("./package.json");
-
-client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 
 client.on("message", async message => {
-
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
-
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
-
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
+  // Let's go with a few common example commands! Feel free to delete or change those.
   
   if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
@@ -99,4 +108,5 @@ client.on("message", async message => {
   }
 });
 
-client.login(config.token);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
