e7e61519fdff6b3c492905719a952263abbf815f
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-14 16:45:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,9 +36,6 @@ client.on('message', message => {
 
      let reason = args.slice(1).join(' ');
      if(!reason) reason = "No reason provided";
-    
-       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
 });
 
