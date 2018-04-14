1c0f400254a3bf0d22c73b8607ccf319ba33c5dc
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-04-14 15:43:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+    client.user.Activity(`Serving ${client.guilds.size} servers`);
 });
 
 
