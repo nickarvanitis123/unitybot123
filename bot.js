df520e3677d81503641625df45e8daefef2fb85b
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-05-20 20:41:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,6 @@ client.on('message', message => {
      let serverembed = new Discord.RichEmbed()
      .setDescription("Server Information")
      .setColor("#15f153")
-     .setThumbnail(sicon)
      .addField("Server Name", message.guild.name)
      .addField("Created On", message.guild.createdAt)
      .addField("You Joined", message.member.joinedAt)
