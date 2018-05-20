9ceaa4bcac10821d46c523d66cf3a577817b923e
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-05-20 19:25:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
    }
     
    if (message.content === '!ping') {
-      message.reply(':cyclone: **pong**');
+      message.message(':cyclone: **pong**');
    }
     
    if (message.content === '!roast') {
