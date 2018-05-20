92419a417cfc8eac36d5c0b1aa9b3c37e75115e1
nickarvanitis123 <38334851+nickarvanitis123@users.noreply.github.com>
2018-05-20 20:37:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,19 +38,18 @@ client.on('message', message => {
 
 
 
-  if(cmd === `${prefix}serverinfo`){
+   if(message.content === '!adf'){
 
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
+     let serverembed = new Discord.RichEmbed()
+     .setDescription("Server Information")
+     .setColor("#15f153")
+     .setThumbnail(sicon)
+     .addField("Server Name", message.guild.name)
+     .addField("Created On", message.guild.createdAt)
+     .addField("You Joined", message.member.joinedAt)
+     .addField("Total Members", message.guild.memberCount);
 
-    return message.channel.send(serverembed);
+     return message.channel.send(serverembed);
   }
     
    if (message.content === '!test') {
